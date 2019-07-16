1ad4cec403f17fb2198e919f54ecdf5549b3b9cd
david <davidweisspost@gmail.com>
2019-07-16 12:59:21 +0200

    Query for stations draft

--- public/app.js
+++ public/app.js
@@ -1,21 +1,24 @@
 const MAPBOX_PUBLIC_KEY = "pk.eyJ1IjoiZGF2aWR3ZWlzc2NvZGUiLCJhIjoiY2p3a3Vpb3p2MHQ2MTN6bjB0ZmZkaWlwcyJ9.cbLzpaovquUhK_TDYFfA3A";
 const KA_POS = {lon: 49.0092, lat: 8.4106};
 
-// https://stackoverflow.com/questions/48535822/es6-module-uncaught-referenceerror-function-is-not-defined-at-htmlbuttonelemen?rq=1
 async function explore() {
-    const response = await fetch("/explore");
-    console.log(response);
-    const data = await response.text();
-    console.log("[C] Response from server is", data);
+    const response = await fetch("/explore?"
+        + "time=" + document.getElementById("timeInput").value
+        + "&lon=" + KA_POS.lon
+        + "&lat=" + KA_POS.lat);
+    const jsonString = await response.text();
+    const exploreData = await JSON.parse(jsonString);
+    console.log("[C] Response from server is", exploreData);
 
-    let marker = L.marker([KA_POS.lon, KA_POS.lat]).addTo(map); // https://leafletjs.com/examples/custom-icons/
+    exploreData.stations.forEach(station => {
+        L.marker([station.lon, station.lat]).addTo(map);
+    });
 
-    let range = L.circle([KA_POS.lon, KA_POS.lat], {
-        color: '#6eb210',
-        fillColor: '#6eb210',
-        fillOpacity: 0.2,
-        radius: document.getElementById("totalTimeInput").value * 0.6 * 60
-    }).addTo(map);
+    // Get a list with all stations within start circle
+
+    // Endpoint /explore?time=60&lon=1&lat=2
+
+
 }
 
 let map = L.map("map").setView([KA_POS.lon, KA_POS.lat], 15);
@@ -25,4 +28,13 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: MAPBOX_PUBLIC_KEY,
+}).addTo(map);
+
+let startMarker = L.marker([KA_POS.lon, KA_POS.lat]).addTo(map); //TODO: https://leafletjs.com/examples/custom-icons/
+
+let startRange = L.circle([KA_POS.lon, KA_POS.lat], {
+    color: '#6eb210',
+    fillColor: '#6eb210',
+    fillOpacity: 0.2,
+    radius: document.getElementById("timeInput").value * 0.6 * 60
 }).addTo(map);
--- server.js
+++ server.js
@@ -8,14 +8,27 @@ require("dotenv").config();
 server.use(express.static("public"));
 
 server.listen(port, () => {
-    console.log(`[S] Listening on port ${port}`); // Use backtick for template literals
+    console.log(`[S] Listening on port ${port}`);
 });
 
-server.get("/explore", (request, response) => {
-    console.log("[S] GET received");
-    // response.json({
-    //     status: "OK",
-    //     msg: "Server says hello!"
-    // });
-    response.send("Server says huhu");
+server.get("/explore", async (request, response) => {
+    console.log("[S] GET received", request.query);
+    let stations = await getStations(); // Wait for time consuming 3rd party server request
+    response.json({
+        status: "OK",
+        msg: "Server says hello!",
+        stations: stations, 
+    });
 });
+
+function getStations() {
+    setTimeout(function() {
+        return dummyStations
+    }, 1000);
+}
+
+let dummyStations = [
+    {lon: 49.0092, lat: 8.4106},
+    {lon: 49.0094, lat: 8.4106},
+    {lon: 49.0099, lat: 8.4106}
+];
